92744f1b2c3669913467001079b433800253c935
mohamed.sobhy <mohamed.sobhy@linkdev.com>
2025-07-22 15:29:56 +0300

    Remove unused components and files

--- src/components/ui/Profile/TabPanel.jsx
+++
@@ -1,8 +0,0 @@
-export default function TabPanel({ id, activeTab, children }) {
-  if (activeTab !== id) return null;
-  return (
-    <div className="p-4 bg-white rounded-lg shadow">
-      {children}
-    </div>
-  );
-}
